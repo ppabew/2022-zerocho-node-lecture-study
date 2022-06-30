157d603d0bc26d32f25de10d325af32c2fc9265a
91274750 <augustman8808@gmail.com>
2022-06-30 18:01:21 +0900

    테스트 코드 추가

--- lecture/HttpTest.js
+++ lecture/HttpTest.js
@@ -1,12 +1,29 @@
 const http = require('http');
+const createNamespace = require('cls-hooked').createNamespace
+const getNamespace = require('cls-hooked').getNamespace
 
+const sessionName = 'cats'
+
+const namespace = createNamespace(sessionName)
+console.log('namespace', namespace)
+
+function someDeepNestedCallToSet() {
+    namespace.run(() => {
+        namespace.set('a', 'value');
+        console.log(namespace);
+    })
+}
+
+function someDeepNestedCallToGet() {
+    const session = getNamespace(sessionName);
+    const baseObject = session.get('a')
+
+    console.log('is working: ', baseObject);
+}
 
 http.createServer((req, res) => {
-    try {
-        throw new Error("에러 발생");
-    } catch (e) {
-        console.log(e)
-    }
+    someDeepNestedCallToSet();
+    someDeepNestedCallToGet()
     console.log('서버 실행');
 }).listen(8081, () => {
     console.log('8081번 포트에서 서버 대기중입니다.')
